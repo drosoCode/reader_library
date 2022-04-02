3bd402ebef1721262075154dd23ed17f9e0d14bd
DrosoCode <internet-site@outlook.com>
2022-04-02 19:40:47 +0200

    fix images

--- main.py
+++ main.py
@@ -6,39 +6,41 @@ import os
 import re
 
 XML_NAMESPACE = {
-    'n':'urn:oasis:names:tc:opendocument:xmlns:container',
-    'pkg':'http://www.idpf.org/2007/opf',
-    'dc':'http://purl.org/dc/elements/1.1/'
+    "n": "urn:oasis:names:tc:opendocument:xmlns:container",
+    "pkg": "http://www.idpf.org/2007/opf",
+    "dc": "http://purl.org/dc/elements/1.1/",
 }
 BASE = "/data"
+
 
 def get_epub_info(fname):
     ret = {}
     tree, path = open_epub(fname)
-    
+
     if tree is None:
         return None
-   
-    p = tree.xpath('/pkg:package/pkg:metadata',namespaces=XML_NAMESPACE)[0]
-    for s in ['title','language','creator','date']:
-        ret[s] = p.xpath('dc:%s/text()'%(s),namespaces=XML_NAMESPACE)[0]
 
-    ret['date'] = ret['date'][:9]
-    
+    p = tree.xpath("/pkg:package/pkg:metadata", namespaces=XML_NAMESPACE)[0]
+    for s in ["title", "language", "creator", "date"]:
+        ret[s] = p.xpath("dc:%s/text()" % (s), namespaces=XML_NAMESPACE)[0]
+
+    ret["date"] = ret["date"][:9]
+
     return ret
+
 
 def open_epub(fname):
     zip = zipfile.ZipFile(fname)
     try:
         try:
-            path = 'OEBPS/'
-            tree = etree.fromstring(zip.read(path+"content.opf"))
+            path = "OEBPS/"
+            tree = etree.fromstring(zip.read(path + "content.opf"))
         except:
-            path = ''
-            tree = etree.fromstring(zip.read(path+"content.opf"))
+            path = ""
+            tree = etree.fromstring(zip.read(path + "content.opf"))
         return (tree, path)
     except:
-        return (None, None)    
+        return (None, None)
 
 
 def get_image(bookPath):
@@ -46,10 +48,18 @@ def get_image(bookPath):
     tree, path = open_epub(bookPath)
     if tree is None:
         return None
-    m = tree.xpath('/pkg:package/pkg:manifest/pkg:item', namespaces=XML_NAMESPACE)
+    m = tree.xpath("/pkg:package/pkg:manifest/pkg:item", namespaces=XML_NAMESPACE)
     for i in m:
-        if i.xpath('@media-type')[0][0:5] == "image" and (i.xpath('@id')[0].find("cover") > -1 or i.xpath('@href')[0].find("cover") > -1):
-            return zip.read(path+i.xpath('@href')[0])
+        if i.xpath("@media-type")[0][0:5] == "image" and (
+            (len(i.xpath("@id")) > 0 and i.xpath("@id")[0].lower().find("cover") > -1)
+            or i.xpath("@href")[0].lower().find("cover") > -1
+            or (
+                len(i.xpath("@properties")) > 0
+                and i.xpath("@properties")[0] == "cover-image"
+            )
+        ):
+            return zip.read(path + i.xpath("@href")[0])
+
 
 def find_first_book(path):
     for i in os.listdir(path):
@@ -60,6 +70,7 @@ def find_first_book(path):
         elif i[-4:] == "epub":
             return os.path.join(path, i)
     return None
+
 
 def list_books(full_path, num_path):
     books = []
@@ -73,13 +84,15 @@ def list_books(full_path, num_path):
     for i in sorted(os.listdir(full_path)):
         p = os.path.join(full_path, i)
         if os.path.isdir(p):
-            books.append({
-                "title": i,
-                "language": "",
-                "creator": "Directory",
-                "date": "",
-                "link":  num_path + str(x)
-            })
+            books.append(
+                {
+                    "title": i,
+                    "language": "",
+                    "creator": "Directory",
+                    "date": "",
+                    "link": num_path + str(x),
+                }
+            )
         elif i[-4:] == "epub":
             data = get_epub_info(p)
             if data is None:
@@ -88,7 +101,7 @@ def list_books(full_path, num_path):
                     "language": "/",
                     "creator": "Unknown",
                     "date": "",
-                    "link":   num_path + str(x)
+                    "link": num_path + str(x),
                 }
             else:
                 data["link"] = num_path + str(x)
@@ -103,6 +116,7 @@ def num_to_path(base, p):
         for i in p.split("/"):
             path = os.path.join(path, sorted(os.listdir(path))[int(i)])
     return path
+
 
 app = Flask(__name__)
 
@@ -125,15 +139,18 @@ def index(path=""):
     p = num_to_path(BASE, path)
 
     if p[-4:] == "epub":
-        return send_file(p, as_attachment=True, download_name=os.path.basename(os.path.normpath(p)), mimetype="application/epub+zip")
+        return send_file(
+            p,
+            as_attachment=True,
+            download_name=os.path.basename(os.path.normpath(p)),
+            mimetype="application/epub+zip",
+        )
     else:
         lst = list_books(p, path)
         return render_template(
-            "index.html",
-            books=lst[page*9:],
-            page=page,
-            max_page=len(lst)//9
+            "index.html", books=lst[page * 9 :], page=page, max_page=len(lst) // 9
         )
+
 
 @app.route("/image/<path:path>")
 def image(path):
@@ -141,6 +158,7 @@ def image(path):
     d = os.path.isdir(p)
     if d:
         p = find_first_book(p)
+
     try:
         img = get_image(p)
     except:
@@ -148,9 +166,10 @@ def image(path):
 
     if img is None:
         return send_file("static/none.png")
-        
+
     response = make_response(img)
-    response.headers.set('Content-Type', 'image/jpeg')
+    response.headers.set("Content-Type", "image/jpeg")
     return response
+
 
 app.run(host="0.0.0.0", port=8080)
